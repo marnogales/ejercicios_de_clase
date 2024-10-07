712c20f63d74ca45241179f8fc9b80e2bedf3328
marnogales <nogales.cantero.mar@iescamas.es>
2024-10-07 10:17:33 +0200

    Add files via upload

---
+++ ejercicio4.py
@@ -0,0 +1,7 @@
+#Esto es un programa para convertir una longitud en metros a kilómetros,centímetros o milímetros.
+print("Dime una longitud en metros que quieras convertir a kilómetros, centímetros o milímetros")
+metro=float(input())
+Kilómetros= (metro / 1000)
+Centímetros= (metro * 100)
+Milímetros= (metro * 1000)
+print("¿A qué cantidad lo quieres pasar?")
