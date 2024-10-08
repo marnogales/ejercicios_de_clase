db133716df757a639d53e8e184ca20901b83ecc4
marnogales <nogales.cantero.mar@iescamas.es>
2024-10-08 11:14:03 +0200

    Delete calculo_del_IMC.py

--- calculo_del_IMC.py
+++
@@ -1,8 +0,0 @@
-#Esto es un programa para el cálculo del IMC
-print("Dime cuál es tu peso en kilogramos")
-peso = float(input())
-print("Genial, ahora dime tu altura en metros")
-altura = float(input())
-#Realizamos la operación
-IMC = peso / (altura * altura) 
-print("Tu IMC es", IMC, "kg/m")
