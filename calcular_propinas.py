6cb703ac16b054567c1c8363926d1cb14678e593
marnogales <nogales.cantero.mar@iescamas.es>
2024-10-08 11:13:37 +0200

    Delete calcular_propinas.py

--- calcular_propinas.py
+++
@@ -1,7 +0,0 @@
-#Esto es un programa para calcular propinas
-print("dime la cantidad de tu factura")
-factura=int(input())
-porcentaje=float(15.0)
-#Realizamos la operación
-propina = factura * (porcentaje / 100) 
-print("la propina es", propina, "€")
